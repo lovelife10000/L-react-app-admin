1b93ca7a657c3cedbc0fbfaebc8deb1096d1fdd0
lovelife10000 <1358180015@qq.com>
2018-06-20 16:13:10 +0800

    使用eslint

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const favicon = require('serve-favicon')
 
 
-const bodyParser = require('body-parser');
-const appConfig = require('../app/config/app');
+const bodyParser = require('body-parser')
+const appConfig = require('../app/config/app')
 
 const app = express()
 
@@ -14,18 +14,18 @@ let port = isDev ? appConfig.devPort : appConfig.prodPort
 
 
 if (isDev) {
-    const config = require('../webpack/dev-client.js')
-    const compiler = require('webpack')(config)
-    app.use(require('webpack-dev-middleware')(compiler, {
-        noInfo: false,
-        hot: true,
-        inline: true,
-        publicPath: config.output.publicPath,
-        stats: {
-            colors: true
-        }
-    }))
-    app.use(require('webpack-hot-middleware')(compiler))
+  const config = require('../webpack/dev-client.js')
+  const compiler = require('webpack')(config)
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: false,
+    hot: true,
+    inline: true,
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }))
+  app.use(require('webpack-hot-middleware')(compiler))
 }
 
 
@@ -33,25 +33,25 @@ app.set('view engine', 'ejs')//开发模式时，采用ejs模版引擎的文件�
 app.set('views', path.join(__dirname, '..', 'dist'))
 app.use(favicon(path.join(__dirname, '..', 'app', 'assets/img/favicon.ico')))
 app.use(express.static(path.join(__dirname, '..', 'dist')))
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }))
 
 
 app.get('*', function (req, res, next) {
-    if (isDev) {
-        res.render('../app/assets/index.ejs');
-    } else {
-        var serverRender = require('../dist/serverRender')
-        serverRender.default(req, res)
-    }
+  if (isDev) {
+    res.render('../app/assets/index.ejs')
+  } else {
+    var serverRender = require('../dist/serverRender')
+    serverRender.default(req, res)
+  }
 
 })
 
 app.listen(port, function (err) {
-    if (err) {
-        console.error(err)
-    } else {
-        console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
-    }
+  if (err) {
+    console.error(err)
+  } else {
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
+  }
 })
 
 
